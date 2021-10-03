073bb2927ef8304e44a5849c3af909e3b28bec4a
martin marchessi <niitram.mm@gmail.com>
2021-10-03 13:51:28 -0300

    descargado de imagenes de los numeros, terminado de pantalla aleatoria y moviemiento del personaje depues del timeout

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ const $nombreJugador2 = document.getElementById("nombreJugador2");
 const $casilleros = document.querySelectorAll("div.casillero");
 const $dado = document.querySelector('#dado');
 const $dadoPantalla = document.querySelector('#dadoPantalla')
+
+class Player {
+    constructor(turno, casillero, ficha, nombre) {
+        this.turno = turno;
+        this.casillero = casillero;
+        this.ficha = ficha;
+        this.nombre = nombre;
+    };
+};
+
+
+const player1 = new Player(true, 0, $ficha1),
+    player2 = new Player(false, 0, $ficha2);
 //Pide los nombres de los jugadores
 const pedirNombre = () => {
     player1.name = prompt(`Ingrese el nombre del primer jugador`)
@@ -19,19 +32,18 @@ const cambiarNombres = () => {
 }
 //toma la posicion en Y
 const getPositionY = (casillero) => {
-    let posicionY = casillero.offsetTop;
-    return posicionY;
+    return casillero.offsetTop;
 };
 //toma la posicion en X
 const getPositionX = (objeto) => {
-    let posicionX = objeto.offsetLeft;
-    return posicionX;
+    return objeto.offsetLeft;
 };
 //encuantra el valor en X del casillero
 const positionXCasillero = (player) => {
     $casilleros.forEach(element => {
         if (element.id === `${player.casillero}`) {
             return posicionXDelCasillero = getPositionX(element)
+
         }
     });
 }
@@ -47,9 +59,19 @@ const positionYCasillero = (player) => {
 const dado = () => {
     return valorDado = Math.floor(Math.random() * (6 - 1 + 1) + 1)
 };
+//calcula y devuelve cual casillero le toca
+const casilleroProximo = (player, dado) => {
+    if (player.casillero >= 100) {
+        return player.casillero = 100
+    }
+    return player.casillero = player.casillero + dado;
+}
+//arreglo que contiene las imagenes de los numeros
+
 //pantallaAleatoria
 const pantallaAleatoria = () => {
     setTimeout(() => {
+        console.log('Random pic')
         $dadoPantalla.setAttribute("src", `img/snake.png`);
     }, 0);
     setTimeout(() => {
@@ -73,13 +95,10 @@ const pantallaAleatoria = () => {
     setTimeout(() => {
         $dadoPantalla.setAttribute("src", `img/signo-pregunta.png`);
     }, 2100);
-}
-//calcula y devuelve cual casillero le toca
-const casilleroProximo = (player, dado) => {
-    if (player.casillero > 100) {
-        return player.casillero = 100
-    }
-    return player.casillero = player.casillero + dado;
+    setTimeout(() => {
+        $dadoPantalla.setAttribute("src", `img/${valorDado}.png`);
+    }, 2400);
+
 }
 //casilleros que suben
 const casillerosSuben = (player) => {
@@ -139,6 +158,12 @@ const casillerosBajan = (player) => {
 const moverFicha = (player, casilleroX, casilleroY) => {
     return casilleroActual = player.ficha.style.transform = `translate(${casilleroX}px, ${casilleroY}px)`;
 };
+//conjunto de las fichas para mover la ficha al casillero 
+const moverFichaPosicion = (player) => {
+    positionXCasillero(player)
+    positionYCasillero(player)
+    moverFicha(player, posicionXDelCasillero, posicionYDelCasillero)
+}
 //player que le toca cambia el borde del nombre
 const turnoActual = (elemento, pixeles = 1, color, size) => {
     elemento.style.border = `${pixeles}px ${color} solid`;
@@ -151,88 +176,63 @@ const agregarClase = (elemento, clase) => {
 }
 
 
-class Player {
-    constructor(turno, casillero, ficha, nombre) {
-        this.turno = turno;
-        this.casillero = casillero;
-        this.ficha = ficha;
-        this.nombre = nombre;
-    };
-};
-
-
-const player1 = new Player(true, 0, $ficha1),
-    player2 = new Player(false, 0, $ficha2);
 /* funciones que ejecuta el player 1 */
 const juegaPlayer1 = () => {
     turnoActual($nombreJugador1, 3, 'rgba(255, 0, 0, 0.192)', 1.3)
-    console.log(`player1: ${player1.casillero}`)
     dado()
-    pantallaAleatoria()
     casilleroProximo(player1, valorDado)
-    positionXCasillero(player1)
-    positionYCasillero(player1)
-    moverFicha(player1, posicionXDelCasillero, posicionYDelCasillero)
-    console.log(`player1: ${player1.casillero}`)
-    let casilleroSubida = casillerosSuben(player1)
-    let casillerosQueBajan = casillerosBajan(player1)
-    console.log(`player1: despues ${player1.casillero}`)
-    if (player1.casillero === casilleroSubida || player1.casillero === casillerosQueBajan) {
-        setTimeout(() => {
-            console.log("desde adentro")
-            positionXCasillero(player1)
-            positionYCasillero(player1)
-            moverFicha(player1, posicionXDelCasillero, posicionYDelCasillero)
-        }, 2000)
-    }
+    pantallaAleatoria()
+    setTimeout(() => {
+        moverFichaPosicion(player1)
+        let casilleroSubida = casillerosSuben(player1)
+        let casillerosQueBajan = casillerosBajan(player1)
+        console.log(`player1: despues ${player1.casillero}`)
+        if (player1.casillero === casilleroSubida || player1.casillero === casillerosQueBajan) {
+            setTimeout(() => {
+                console.log("desde adentro")
+                moverFichaPosicion(player1)
+            }, 2000)
+        }
+        player1.turno = false;
+        player2.turno = true;
+        $dado.removeEventListener('click', juegaPlayer1)
+        $dado.addEventListener('click', juegaPlayer2)
+    }, 2500);
     $ficha1.addEventListener("transitionend", () => {
         turnoActual($nombreJugador2, 3, 'rgba(21, 255, 0, 0.192)', 1.3)
         turnoActual($nombreJugador1, 0, 'rgba(255, 255, 255, 0)', 0.8)
     });
-    console.log(`player1 despues del interval: ${player1.casillero}`)
-    player1.turno = false;
-    player2.turno = true;
-    $dado.removeEventListener('click', juegaPlayer1)
-    $dado.addEventListener('click', juegaPlayer2)
 }
 /* funciones que ejecuta el player 2 */
 const juegaPlayer2 = () => {
-    console.log(`player2: ${player2.casillero}`)
     dado()
     casilleroProximo(player2, valorDado)
-    positionXCasillero(player2)
-    positionYCasillero(player2)
-    moverFicha(player2, posicionXDelCasillero, posicionYDelCasillero)
-    console.log(`player2: ${player2.casillero}`)
-    let casilleroSubida = casillerosSuben(player2)
-    let casillerosQueBajan = casillerosBajan(player2)
-    console.log(`player2: despues ${player2.casillero}`)
-    if (player2.casillero === casilleroSubida || player2.casillero === casillerosQueBajan) {
-        setTimeout(() => {
-            console.log("desde adentro")
-            positionXCasillero(player2)
-            positionYCasillero(player2)
-            moverFicha(player2, posicionXDelCasillero, posicionYDelCasillero)
-        }, 2000)
-    }
-    $ficha2.addEventListener("transitionend", () => {
-        turnoActual($nombreJugador1, 3, 'rgba(255, 0, 0, 0.192)', 1.3)
-        turnoActual($nombreJugador2, 0, 'rgba(255, 255, 255, 0)', .8)
-    });
-    console.log(`player2 despues del interval: ${player2.casillero}`)
-    player2.turno = false;
-    player1.turno = true;
-    $dado.removeEventListener('click', juegaPlayer2)
-    $dado.addEventListener('click', juegaPlayer1)
+    pantallaAleatoria()
+    setTimeout(() => {
+        moverFichaPosicion(player2)
+        console.log(`player2: ${player2.casillero}`)
+        let casilleroSubida = casillerosSuben(player2)
+        let casillerosQueBajan = casillerosBajan(player2)
+        console.log(`player2: despues ${player2.casillero}`)
+        if (player2.casillero === casilleroSubida || player2.casillero === casillerosQueBajan) {
+            setTimeout(() => {
+                console.log("desde adentro")
+                moverFichaPosicion(player2)
+            }, 2000)
+        }
+        $ficha2.addEventListener("transitionend", () => {
+            turnoActual($nombreJugador1, 3, 'rgba(255, 0, 0, 0.192)', 1.3)
+            turnoActual($nombreJugador2, 0, 'rgba(255, 255, 255, 0)', .8)
+        });
+        console.log(`player2 despues del interval: ${player2.casillero}`)
+        player2.turno = false;
+        player1.turno = true;
+        $dado.removeEventListener('click', juegaPlayer2)
+        $dado.addEventListener('click', juegaPlayer1)
+    }, 2500);
 }
 /* EMPIEZA EJECUCION DE FUNCIONES */
-const empezar = new Promise(
-    pantallaAleatoria()
-);
-$dado.addEventListener('click', () => {
-
-    empezar.then(juegaPlayer1)
-})
+$dado.addEventListener('click', juegaPlayer1)
 
 window.onload = function () {
     pedirNombre();
